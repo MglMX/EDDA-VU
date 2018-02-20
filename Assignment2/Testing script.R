41c664ddc8aa22f115b9fd336ddab614ed73651d
Miguel.Morales <m.e.miguel@student.vu.nl>
2018-02-20 19:23:05 +0100

    Changes made after meeting!

--- Assignment2/Testing script.R
+++ Assignment2/Testing script.R
@@ -1,3 +1,4 @@
+par(mfrow=c(1,1));
 #Exercise 1
 #1.1
 Data <- read.table("telephone.txt",header = TRUE);
@@ -5,29 +6,48 @@ bills = Data$Bills;
 
 t=median(bills)
 t
-
-hist(bills,prob=T);
-
-B=10000;
-tstar=numeric(B);
 n=length(bills);
 n
 
-for(i in 1:B){
-  xstar = rexp(n,0.1); #Exercise says for lambda = [0.01,0.1]
-  tstar[i] = median(xstar);
+hist(bills,prob=T);
+
+bootstrap_exp<-function(n,lambda){
+  B=1000;
+  tstar=numeric(B);
+  for(i in 1:B){
+    xstar = rexp(n,lambda); #Exercise says for lambda = [0.01,0.1]
+    tstar[i] = median(xstar);
+  }
+  return(tstar)
 }
 
-hist(tstar,prob=T);
+lambda_values=seq(0.01,0.1,length=500)
+lambda_values
 
-pl=sum(tstar<t)/B;
-pr=sum(tstar>t)/B;
-p=2*min(pl,pr);
-p;pl;pr;
+p_array=numeric(length(lambda_values))
+i=1
+for(lambda in lambda_values){
+  bootstrap_exp(n,lambda);
+  pl=sum(tstar<t)/B;
+  pr=sum(tstar>t)/B;
+  p=2*min(pl,pr);
+  p_array[i]=p;
+  i=i+1;
+}
 
-#With a p value of 0 we reject the hypothesis that the distribution is exponential
+p_array;
+
+length(lambda_values)
+length(p_array)
+
+plot(lambda_values,p_array)
+#Checking our graph we get that forthe values of lambda in range [0.01,0.1] all the p-values we get are 0. so we can reject the H0 that our distribution come from an exponential distribution of lambda range 0.01 0.1
+
 #1.2
+
 hist(bills)
+boxplot(bills)
+#Since half of the customers spend less than 27€ the marketing advice could be 
 #Any other plot?
 #Conclusion: Most of the people spend less than 20€ per month (?)
 
@@ -52,7 +72,7 @@ light_speed
 
 #2.1
 #Histograms and box plots
-par(mfrow=c(1,2));
+par(mfrow=c(1,3));
 hist(light1879);
 boxplot(light1879);
 
@@ -62,7 +82,14 @@ boxplot(light1882);
 hist(light_speed);
 boxplot(light_speed);
 
-#Obersvations: ??
+
+boxplot(light1879,light1882,light_speed)
+
+quantile(light1879)
+quantile(light1882)
+quantile(light_speed)
+#Comparing the histograms and boxplots we can see that the distribution of the light1882 and light_speed are quite similar. Meanwhile, the light1879 the boxplot seems shifted upwards which means that the values are bigger.
+#If we consider the median values then we get for light1879 is 850, for 1882 is 774 and light_speed is 754. We see again that light1882 and light_speed medians are close to each other.
 
 #2.2
 #Bootstrap confidence intervals lecture 2
@@ -94,33 +121,37 @@ get_confidence_intervals<-function(data,operation="mean"){
   tstar = bootstrap(data,operation);
   tstar25 = quantile(tstar,0.025);
   tstar975 = quantile(tstar,0.975);
-  return(c(2*T1-tstar975,2*T1-tstar25));
+  return(c(2*t-tstar975,2*t-tstar25));
 }
 
 #Light1879
+add_speed=299000
+add_speed=0
+
+
 confidence_mean = get_confidence_intervals(light1879);
-confidence_mean+299000;
+confidence_mean+add_speed;
 
 confidence_median = get_confidence_intervals(light1879,"median");
-confidence_median+299000;
+confidence_median+add_speed;
 
 #Light1882
 confidence_mean = get_confidence_intervals(light1882);
-confidence_mean+299000;
+confidence_mean+add_speed;
 
 confidence_median = get_confidence_intervals(light1882,"median");
-confidence_median+299000;
+confidence_median+add_speed;
 
 #Light
 confidence_mean = get_confidence_intervals(light_speed);
-confidence_mean+299000;
+confidence_mean+add_speed;
 
 confidence_median = get_confidence_intervals(light_speed,"median");
-confidence_median+299000;
+confidence_median+add_speed;
 
 #2.3 Comments
-#??
-
+#We see that for the confidence intervals the light1882 and lightspeed they are similir and more or less in the same range. Meanwhile, the confidence interval of 1879 is not in the same range as the other two for the mean and for the median. The intesection of the intervals of 1879 and the others
+#is empty.Then the experiments of 1879 maybe are not accurate enough or at least they differ from the other two experiments..
 #2.4
 #Speed of light according to https://physics.nist.gov/cgi-bin/cuu/Value?c
 #299792.458
@@ -129,12 +160,16 @@ confidence_median+299000;
 # For light it also doesn't fall in the interval neither mean or median
 #It is not consistent
 
+#We see that the currently most accurate value of speed does not fall in the confidence interval of the first data set (1879) neither does in the measures of Newcombs (light_speed) neither for the mean or median. But, it does falls in the mean and median confidence interval of Michael's 1882.
+#It is consistent for Michelson measurements of the experiments made in 1882 but not in the other two.
+
 #Exercise 3
 #3.1
 klm = scan("klm.txt");
 klm
 hist(klm)
 qqnorm(klm)
+qqline(klm)
 
 #We check that the population doesn not seem normal this tell us that doing the t-test is not the best option
 #We should perform the sign test Lecture 3 (13)
@@ -142,25 +177,29 @@ qqnorm(klm)
 
 #Sign test
 #We compute how many values are bigger than the median to test
-n_bigger = sum(31>=klm)
+n_smaller = sum(klm>31)
+n_smaller
 n = length(klm)
-binom.test(n_bigger,n,p=0.5)
+n
+binom.test(n_smaller,n,p=0.5)
 
 median(klm) #Checking
+quantile(klm)
 
-
-#We can reject the null hypothesus that the median of the population is == 31. 
+#
+#We can reject the null hypothesus that the median of the population is == 31. But checking at the confidence interval 0.53 0.78, we see that 31 should be between quantile 0.53 and 0.78 therefore 
 # How to change the null hypothesis to check if population is <= than 31?
 # Checking confidence interval it gives 0.21 and 0.46. Can we say with that info the median is bigger than 31?
 
-#3.2
-quantile(klm,probs=0.9) #$Just checking
 
-n_bigger = sum(72>klm);
-binom.test(n_bigger,n,p=0.9)
+#3.2
+quantile(klm,probs=0.8) #$Just checking
+
+n_bigger = sum(klm>72);
+binom.test(n_bigger,n,p=0.1)
 
 #With a p value of 0.002 we can reject that at most 10% of the parts arrive later than 72 days. 
-# If we check the confidence intervals 0.64 and 0.87, this means that between 13% and 36% of the pieces arrive later than 72 days (Can we say this?)
+# If we check the confidence intervals 0.12 and 0.34, this means that between 12% and 34% of the pieces arrive later than 72 days (Can we say this?)
 
 #Exercise 4
 Clouds<-read.table("clouds.txt",header = TRUE);
@@ -169,6 +208,7 @@ hist(Clouds$seeded)
 hist(Clouds$unseeded)
 
 differences=Clouds$seeded-Clouds$unseeded
+mean(differences)
 
 hist(differences)
 boxplot(Clouds$seeded,Clouds$unseeded)
@@ -181,7 +221,8 @@ qqnorm(differences)
 qqline(differences)
 shapiro.test(differences)
 #QQplot an shapiro indicate that the differences do not follow normal distribution
-#Maybe check something about order of the values??
+
+#Should we do t.test eventhough the differences of the two data sets doesn't seem to be normal
 
 #T-test
 #Lecture 3 slide 25 says that paired t-test assumes that the differneces are a random sample from a normal population
@@ -195,10 +236,12 @@ t.test(Clouds$seeded,Clouds$unseeded,paired = TRUE)
 #Mann-whitney-wilcoxon test. Lecture 4 slide 20
 wilcox.test(Clouds$seeded,Clouds$unseeded)
 #p-value indicates that H0 is rejected. The samples come from different populations
+#though probably we shouldn't use this test because it is for independent samples and our data is paired
 
 #Kolmorov-Smirnov. Lecture 4 slide 22
 ks.test(Clouds$seeded,Clouds$unseeded);
 #p-value indicates that H0 is rejected. The samples come from different populations
+#though probably we shouldn't use this test because it is for independent samples and our data is paired
 
 #4.2
 clouds_sq=sqrt(Clouds)
