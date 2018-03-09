347bdf3e63c30de322982337dbb37b8de37e89bc
tanjina.islam <t.islam@student.vu.nl>
2018-03-09 15:29:05 +0100

    Exercise 5 updated

--- Assignment3/Exercise 5.R
+++ Assignment3/Exercise 5.R
@@ -7,20 +7,62 @@ nauseadata = read.table("nauseatable.txt", header = TRUE)
 attach(nauseadata)
 nauseadata
 
-nausea = as.vector(rep(0:1, 152)) # 304 or 152 or ??
-nausea
-medicin = as.vector(rep(1:2, each = 152)) # I have no idea about how to define medicin labels!!
-medicin
+nonausea_detected = nauseadata[, 1]
+nonausea_detected
 
-nausea.frame = data.frame(nausea, medicin)
+nausea_detected = nauseadata[, 2]
+
+#nausea = as.vector(rep(0:1, 152)) # 304 or 152 or ??
+nausea_detected
+
+nausea = as.vector(rep(1, each = nausea_detected[1]+nausea_detected[2]+nausea_detected[3]))
+nausea
+
+nonausea = as.vector(rep(0, each = nonausea_detected[1]+nonausea_detected[2]+nonausea_detected[3]))
+nonausea
+
+nausea_vector = c(nonausea, nausea)
+nausea_vector
+
+Chlorpromazine_nonausea = as.vector(rep("C", 100))
+Chlorpromazine_nonausea
+
+Pentobarbital_nonausea_1 = as.vector(rep("P1", 32))
+Pentobarbital_nonausea_1
+
+Pentobarbital_nonausea_2 = as.vector(rep("P2", 48))
+Pentobarbital_nonausea_2
+
+Chlorpromazine_nausea = as.vector(rep("C", 52))
+Chlorpromazine_nausea
+
+Pentobarbital_nausea_1 = as.vector(rep("P1", 35))
+Pentobarbital_nausea_1
+
+Pentobarbital_nausea_2 = as.vector(rep("P2", 37))
+Pentobarbital_nausea_2
+
+medicin_vector = c(Chlorpromazine_nonausea, Pentobarbital_nonausea_1, Pentobarbital_nonausea_2, Chlorpromazine_nausea, Pentobarbital_nausea_1, Pentobarbital_nausea_2)
+medicin_vector
+
+
+# medicin = as.vector(rep(1:2, each = 152)) # I have no idea about how to define medicin labels!!
+# medicin
+
+nausea.frame = data.frame(nausea_vector, medicin_vector)
 # nausea.frame If I try to print all data from that frame I'm getting more than 304 rows why??
-nausea.frame[1:304,]
+
+nausea.frame
 
 
 ##T2
-xtabs(~medicin+nausea, data = nausea.frame)
+xtabs(~medicin_vector+nausea_vector, data = nausea.frame)
+chisq.test(xtabs(~medicin_vector+nausea_vector, data = nausea.frame))[[1]]
+
+# In this case R does not give the warning, since the approximation is reliable for
+# nausea_vector and medicin_vector in nausea.frame
 
 ##T3
-
+# Check Lecture 5 page : 1-12
 
 ##T4
