111b8d9e5572da9dc84c26ee64d597bfda3005ad
tanjina.islam <t.islam@student.vu.nl>
2018-03-09 11:53:37 +0100

    Exercise-5 - initial commit

--- Assignment3/Exercise 5.R
+++ Assignment3/Exercise 5.R
@@ -1,10 +1,24 @@
 ###EXERCISE 5
+library(multcomp)
+library(lme4)
 
 ##T1
+nauseadata = read.table("nauseatable.txt", header = TRUE)
+attach(nauseadata)
+nauseadata
+
+nausea = as.vector(rep(0:1, 152)) # 304 or 152 or ??
+nausea
+medicin = as.vector(rep(1:2, each = 152)) # I have no idea about how to define medicin labels!!
+medicin
+
+nausea.frame = data.frame(nausea, medicin)
+# nausea.frame If I try to print all data from that frame I'm getting more than 304 rows why??
+nausea.frame[1:304,]
 
 
 ##T2
-
+xtabs(~medicin+nausea, data = nausea.frame)
 
 ##T3
 
