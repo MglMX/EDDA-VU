d5c0631de6ede103d3465a7989f801c0bf902734
Miguel.Morales <m.e.miguel@student.vu.nl>
2018-03-09 12:49:54 +0100

    Exercise 2

--- Assignment3/Exercise 2.R
+++ Assignment3/Exercise 2.R
@@ -9,6 +9,7 @@ attach(search)
 #like in the last exercise
 
 
+
 ##T2
 interaction.plot(skill,interface,time)
 interaction.plot(interface,skill,time)
@@ -20,22 +21,52 @@ interaction.plot(interface,skill,time)
 search$skill = as.factor(search$skill)
 search$interface = as.factor(search$interface)
 
-searchaov = lm(time~interface,data=search)
+searchaov = lm(time~interface+skill,data=search)
 anova(searchaov)
 
-#H0 mu0=mu1=mu2=mu3=mu4 cannot be rejected.
-#I used 1 way anova because searchaov = lm(time~interface*skill,data=search) was not returning p-value.
+#H0 mu0=mu1=mu2=mu3=mu4 can be rejected.
+#So the time is not the same for all the interfaces.
 
 
 ##T4
+#Lecture 6 (3)
 
+library(multcomp)
+searchaov2 = lm(time~interface+skill,data=search)
+
+searchmult = glht(searchaov2,linfct=mcp(skill="Tukey"))
+summary(searchmult)
+
+#According to the test 4 -3 == 0 has an estimate of 2.267. That's the estimte time for a user of skill 4 with the interface 3.
 
 ##T5
+#Lecture 5  (26-27)
 
+qqnorm(residuals(searchaov2))
+qqline(residuals(searchaov2))
+#The QQ-plot seems a bit deviated in the extremes but it could be normal
+
+plot(fitted(searchaov2),residuals(searchaov2))
+
+#We see that the doesn't change systematically with the fitted values.
+#So we can assume that the population comes from where the data comes from has equal variances.
 
 ##T6
-
+#Lecture 6 (31)
+friedman.test(search$time,search$interface,search$skill)
+#We reject H0 (Interface doesn't have an effect) so we can say that the interface makes an effect.
 
 ##T7
+search$interface = as.factor(search$interface)
 
+searchaov = lm(time~interface,data=search)
 
+anova(searchaov)
+
+#According to p-value we cannot reject that the means are the same for the different interfaces.
+#It is not useful because the variable skill should also be considered
+
+#1-way-anova assumes that the data come from normal population and the variances are equal.
+qqnorm(residuals(searchaov))
+qqline(residuals(searchaov))
+#The population doesn't seem normal therefore the assumption is not met.
