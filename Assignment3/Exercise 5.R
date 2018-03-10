968ca03d8ddd924e4d45298e64ba0ddf775aaa8a
tanjina.islam <t.islam@student.vu.nl>
2018-03-10 13:59:54 +0100

    Exercise 5 completed

--- Assignment3/Exercise 5.R
+++ Assignment3/Exercise 5.R
@@ -53,11 +53,13 @@ nausea.frame = data.frame(nausea_vector, medicin_vector)
 # nausea.frame If I try to print all data from that frame I'm getting more than 304 rows why??
 
 nausea.frame
+# head(nausea.frame)
+# tail(nausea.frame)
 
 
 ##T2
 xtabs(~medicin_vector+nausea_vector, data = nausea.frame)
-chisq.test(xtabs(~medicin_vector+nausea_vector, data = nausea.frame))[[1]]
+
 
 # In this case R does not give the warning, since the approximation is reliable for
 # nausea_vector and medicin_vector in nausea.frame
@@ -65,4 +67,36 @@ chisq.test(xtabs(~medicin_vector+nausea_vector, data = nausea.frame))[[1]]
 ##T3
 # Check Lecture 5 page : 1-12
 
+mystat = function(x) sum(residuals(x)^2)
+B = 1000
+tstar =  numeric(B)
+for (b in 1:B) {
+ medicinstar = sample(medicin_vector)  # Permutting the medicin labels
+ tstar[b] = mystat(lm(nausea_vector~medicinstar))
+}
+
+myt = mystat(lm(nausea_vector~medicin_vector))
+
+hist(tstar)
+myt # value = 71.82106
+
+pl = sum(tstar<myt)/B
+pl # p-value = 0.032
+pr = sum(tstar>myt)/B
+pr # p-value = 0.967
+2*pl # 2*p-value = 0.064
+
+chisq.test(xtabs(~medicin_vector+nausea_vector, data = nausea.frame))
+
+# p-value = 0.03643
+
+chisq.test(xtabs(~medicin_vector+nausea_vector, data = nausea.frame))[[1]]
+
+# x-squared = 6.624765
+
+### Findings: I don't know what should we say/conclude here for this task!
+
 ##T4
+
+## If we look at the p-values for both permutation test and chisq test we can seee that,
+## for both of the tests we have same p-values that is 0.03
