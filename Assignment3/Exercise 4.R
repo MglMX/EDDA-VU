92443843af2c887a6a251f111dc35af8ccd6d768
carlosperales95 <carlos.perales95@opendeusto.es>
2018-03-11 17:04:01 +0100

    Add files via upload
    Almost all exercise 4 and first try to exercise 7 (step-up method)

--- Assignment3/Exercise 4.R
+++ Assignment3/Exercise 4.R
@@ -1,12 +1,49 @@
+library(multcomp)
+library(lme4)
+
 ###EXERCISE 4
+
+cows = read.table("cow.txt", header = TRUE)
+boxplot(cows)
+stripchart(cows, vertical=TRUE)
+
 
 ##T1
 
+#cowframe=data.frame(treatment=as.factor(cows$treatment), milk=as.vector(cows$milk))
+#cowframe[1:18,]
+#cowsaov=lm(milk~treatment,data=cowframe)
+#summary(cowsaov)
+
+#cows$id=factor(cows$id)
+#cowsaov=lm(milk~treatment+id,data=cows)
+#anova(cowsaov)
+#summary(cowsaov)
+
+cows$id=factor(cows$id)
+cows$per=factor(cows$per)
+cowslm=lm(milk~treatment+per+id,data=cows)
+summary(cowsaov)
 
 ##T2
 
 
+
 ##T3
 
+#still don't really know how this works, we have to specify a random effect with | somehow but I still have o idea how to manage that
+cowsaover=lmer(milk|treatment,data=cowframe)
+summary(cowsaover)
+
+cowslmer=lmer(milk~treatment+order+per+(1|id),data=cows,REML=FALSE)
+summary(cowslmer)
 
 ##T4
+
+attach(cows)
+t.test(milk[treatment=="A"],milk[treatment=="B"],paired=TRUE)
+
+#we can't reject that the difference is = to 0
+#is this a good test? just show difference of means
+
+
--- Assignment3/Exercise 7.R
+++ Assignment3/Exercise 7.R
@@ -1,1 +1,54 @@
 ###EXERCISE 7
+
+library(multcomp)
+library(lme4)
+
+crime =  read.table("expensescrime.txt", header=TRUE)
+crime
+plot(crime[,c(1,4,7)])
+##stepup method
+#step1
+crimelm=lm(expend~bad,data=crime)
+summary(crimelm)
+
+crimelm=lm(expend~crime,data=crime)
+summary(crimelm)
+
+crimelm=lm(expend~lawyers,data=crime)
+summary(crimelm)
+
+#we choose this one
+crimelm=lm(expend~employ,data=crime)
+summary(crimelm)
+
+crimelm=lm(expend~pop,data=crime)
+summary(crimelm)
+
+#step2
+crimelm=lm(expend~employ+bad,data=crime)
+summary(crimelm)
+
+crimelm=lm(expend~employ+crime,data=crime)
+summary(crimelm)
+#choose this
+crimelm=lm(expend~employ+lawyers,data=crime)
+summary(crimelm)
+
+crimelm=lm(expend~employ+pop,data=crime)
+summary(crimelm)
+
+#step3
+
+crimelm=lm(expend~employ+lawyers+bad,data=crime)
+summary(crimelm)
+
+crimelm=lm(expend~employ+lawyers+pop,data=crime)
+summary(crimelm)
+
+crimelm=lm(expend~employ+lawyers+crime,data=crime)
+summary(crimelm)
+
+##adding any of these = significant explanatory variables only +0.001
+
+#The resulting model of the step-up method is
+#total = 993.8317 + 12.2865*expend - 2.8509*takers + error
