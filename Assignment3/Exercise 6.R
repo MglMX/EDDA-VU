fe20f7a0652404b84079495ac50f463f91145b03
Miguel.Morales <m.e.miguel@student.vu.nl>
2018-03-13 19:31:20 +0100

    Exercise 1 and 6

--- Assignment3/Exercise 6.R
+++ Assignment3/Exercise 6.R
@@ -8,68 +8,70 @@ pairs(pollution)
 #There seem to be correlation between wind and humidity, Temperature and oxidant.
 #There seem to be negative correlation between wind and temperature, temperature and humidity, wind and oxidant.
 
+round(cor(pollution[,2:6]),2)
 
 ##T2
 
 #First we perform the test on each of the explanatory variables to get the best.
 poplm = lm(oxidant~wind,data=pollution)
 summary(poplm)
-qqnorm(residuals(poplm))
-qqline(residuals(poplm))
-plot(fitted(poplm),residuals(poplm))
-#p-value 8.20e-07 and the assumptions seem true according to qqplot and plot
+#R2 = 0.59
 
 poplm = lm(oxidant~temperature,data=pollution)
 summary(poplm)
-qqnorm(residuals(poplm))
-qqline(residuals(poplm))
-plot(fitted(poplm),residuals(poplm))
-#p-value 1.17e-06 and the assumptions seem true according to qqplot and plot
+#R2 = 0.58
 
 poplm = lm(oxidant~humidity,data=pollution)
 summary(poplm)
-qqnorm(residuals(poplm))
-qqline(residuals(poplm))
-plot(fitted(poplm),residuals(poplm))
-#p-value 0.056 and the assumptions are doubtful.
+#R2 = 0.12
 
 poplm = lm(oxidant~insolation,data=pollution)
 summary(poplm)
-qqnorm(residuals(poplm))
-qqline(residuals(poplm))
-plot(fitted(poplm),residuals(poplm))
-#p-value 0.004 and assumptions are doubtful
+#R2 = 0.26
 
-#The variable with lower p-value is wind
-#The linear regression model could be: Yn = 45.31 - 0.63*wind
-
+#The variable that gives higher R2 is wind 
+#We add another variable
 
 poplm = lm(oxidant~wind+temperature,data=pollution)
 summary(poplm)
-qqnorm(residuals(poplm))
-qqline(residuals(poplm))
-plot(fitted(poplm),residuals(poplm))
-#The assumptions seem true
-#The linear regression model could be: Yn = -5.2 - 0.43*wind + 0.52*temperature
+#R2 = 0.78
+
+poplm = lm(oxidant~wind+humidity,data=pollution)
+summary(poplm)
+#R2 = 0.59
+
+poplm = lm(oxidant~wind+insolation,data=pollution)
+summary(poplm)
+#R2 = 0.66
+
+#The one with higer value is wind + temperature with R2 of 0.78
+poplm = lm(oxidant~wind+temperature+humidity,data=pollution)
+summary(poplm)
+#R2 = 0.79
 
 poplm = lm(oxidant~wind+temperature+insolation,data=pollution)
 summary(poplm)
-qqnorm(residuals(poplm))
-qqline(residuals(poplm))
-plot(fitted(poplm),residuals(poplm))
-#Insolation doesn't seem to be correlated. so this extension is not useful
+#R2 = 0.78
+
+#None of these are relevant for hte linear regression.
+#The final linear regression model could be: Yn = -5.0 -0.43*wind + 0.52*temperature
+
 
 ##T3
 poplm = lm(oxidant~wind+temperature+humidity+insolation,data=pollution)
 summary(poplm)
-qqnorm(residuals(poplm))
-qqline(residuals(poplm))
-plot(fitted(poplm),residuals(poplm))
-#According to the p-value, we should decrease the full model not considering humidity and insolation.
-#The result linear model would be: Yn = -15.49 - 0.44*wind + 0.56*temperature
+#The one with highest p-value is insolation
+#We delete insolation from the model
 
+poplm = lm(oxidant~wind+temperature+humidity,data=pollution)
+summary(poplm)
+#The one with highest p-value is humidity.
+#We delete it from the model
 
+poplm = lm(oxidant~wind+temperature,data=pollution)
+summary(poplm)
 
+#The final model could be Yn = -5.0 -0.43*wind + 0.52*temperature
 
 ##T4
 #Estimates: wind = 0.43, temperature = 0.52, humidity = 0, insolation = 0;
