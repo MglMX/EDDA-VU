1f6b8b1d8973a8ed610794b221b4d68cd4914241
tanjina.islam <t.islam@student.vu.nl>
2018-03-14 16:56:18 +0100

    reviewed ex-4 and added comments

--- Assignment3/Exercise 4.R
+++ Assignment3/Exercise 4.R
@@ -23,25 +23,35 @@ stripchart(cows, vertical=TRUE)
 cows$id=factor(cows$id)
 cows$per=factor(cows$per)
 cowslm=lm(milk~treatment+per+id,data=cows)
-summary(cowsaov)
+summary(cowslm)
+
+## Findings check lecture 7 slide: 11-12
+
+
+cowsanova=lm(milk~id+per+treatment,data=cows)
+anova(cowsanova) # IDK whether we should include it or not! Check lec 7 slide:12
 
 ##T2
 
-
+# Estimates of treatment B = -0.5100
+# Estimates of perid 2 = -2.3900
 
 ##T3
 
 #still don't really know how this works, we have to specify a random effect with | somehow but I still have o idea how to manage that
-cowsaover=lmer(milk|treatment,data=cowframe)
+cowsaover=lmer(milk|treatment,data=cowframe) ## This isn't required maybe!
 summary(cowsaover)
 
 cowslmer=lmer(milk~treatment+order+per+(1|id),data=cows,REML=FALSE)
-summary(cowslmer)
+summary(cowslmer) ## For explanation.... [Check lecture 7 slide: 13]
+
+cowslmer1 = lmer(milk~order+per+(1|id), data=cows, REML=FALSE)
+anova(cowslmer1, cowslmer) ## p-value = 0.446 for cowslmer [Check lecture 7 slide: 14]
 
 ##T4
 
 attach(cows)
-t.test(milk[treatment=="A"],milk[treatment=="B"],paired=TRUE)
+t.test(milk[treatment=="A"],milk[treatment=="B"],paired=TRUE) ## p-value = 0.8281
 
 #we can't reject that the difference is = to 0
 #is this a good test? just show difference of means
