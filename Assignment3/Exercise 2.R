285f1bec1134e91904ef9d854067520b92eaddb7
Miguel.Morales <m.e.miguel@student.vu.nl>
2018-03-14 14:11:41 +0100

    Exercise 2 Task 1

--- Assignment3/Exercise 2.R
+++ Assignment3/Exercise 2.R
@@ -9,8 +9,10 @@ B=5
 
 for (i in 1:B){
   print(sample(1:(N*I)))
-} 
-#The first student of level 1 will use the itnerface 2, the second one will use interface 1 and the third one interface 3.
+}
+
+#The rows in the matrix represent the skill level of the students. Row 1 is level 1, row 2 is level 2, etc.
+#The first student of level 1 will use the interface 2, the second one will use interface 1 and the third one interface 3.
 #For the students of level 3, first student will use interface 3, 2nd student will use interface 1 and third student interface 2,
 #And so on.
 
@@ -30,8 +32,9 @@ search$interface = as.factor(search$interface)
 searchaov = lm(time~interface+skill,data=search)
 anova(searchaov)
 
-anova(searchaov)[[5]][1]
-#H0 mu0=mu1=mu2=mu3=mu4 can be rejected iwht a p-value of 0.01310.
+anova(searchaov)[[5]][1] #p-value interface
+
+#H0 mu0=mu1=mu2=mu3=mu4 can be rejected with a p-value of 0.01310.
 #So the time is not the same for all the interfaces.
 
 
@@ -42,8 +45,6 @@ library(multcomp)
 searchaov2 = lm(time~interface+skill,data=search)
 
 searchmult = glht(searchaov2,linfct=mcp(skill="Tukey"))
-summary(searchmult)
-
 summary(searchmult)
 
 #According to the test 4 -3 == 0 has an estimate of 2.267. That's the estimte time for a user of skill 4 with the interface 3.
