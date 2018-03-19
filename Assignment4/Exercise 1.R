94d96a9e498f9e0a111346148794e360ff4f00d6
Miguel.Morales <m.e.miguel@student.vu.nl>
2018-03-19 13:16:39 +0100

    Exercise 1 5-6

--- Assignment4/Exercise 1.R
+++ Assignment4/Exercise 1.R
@@ -44,12 +44,44 @@ summary(fliesanova)
 # Isolated 38.72 - 18 = 20.72
 
 #T5
+fliesframe$activity = as.factor(fliesframe$activity)
+fliesframe$thorax = as.factor(fliesframe$thorax)
 
+fliesanova2 = lm(longevity~activity+thorax,data = fliesframe) #Should I use * or +
+anova(fliesanova2)
 
+#According to the p-value < 0.05 activity it has a maiun effect on longevity
+
+#The same using *
+fliesanova2 = lm(longevity~activity*thorax,data = fliesframe) #Should I use * or +
+anova(fliesanova2)
+
+#According to the p-value < 0.05 activity it has a maiun effect on longevity
 
 #T6
+contrasts(fliesframe$thorax)=contr.sum
+contrasts(fliesframe$activity)=contr.sum
+summary(fliesanova2)
+
+#According to the estimates, too much sexual activity (high) doesn't increase longevity as much as low sexual activity. Isolated flies die faster.
+#Estimates:
+#Activity1(isolated) = -14.05
+#Activity2(low) =  14.11
+#Activity3(high)= -(-14.05+14.11) = -0.6
+#! Not sure that activity1 correspond to isolated, activity2 to low and activity3 to high
+
+fliesframe$thorax = as.numeric(fliesframe$thorax)
+average_thorax = mean(fliesframe$thorax) #Obatining mean for the thorax
+average_thorax #8.87
+fliesframe$thorax = as.factor(fliesframe$thorax)
 
 
+#Estimates:
+#Activity1(isolated):thorax8 = -0.28
+#Activity2(low):thorax8 = -9.44
+#Activity3(high):thorax8 = -(-0.28-9.44) = 9.72
+#! Not sure that activity1 correspond to isolated, activity2 to low and activity3 to high
+#! Also not sure thorax8 means that those are the flies with thorax length 8 (which is the average)
 
 #T7
 
