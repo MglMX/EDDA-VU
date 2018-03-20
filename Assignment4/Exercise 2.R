32905f870180258152e3e730bdde9876a1767213
carlosperales95 <carlos.perales95@opendeusto.es>
2018-03-20 15:43:02 +0100

    all exercise 5 but task 5

--- Assignment4/Exercise 2.R
+++ Assignment4/Exercise 2.R
@@ -1,14 +1,19 @@
 #Exercise 2
 
+#First we load the data from the data source
 expsi = read.table("psi.txt", header=TRUE);
 expsi
 
 #T1
 
+#We divide the data in 2 groups, 
+#the students that received psi and the ones that didn't
 nousepsi = expsi[which(expsi$psi == "0"),]
 nousepsi
 usepsi = expsi[which(expsi$psi == "1"),]
 usepsi
+
+# We show some data summaries
 
 hist(nousepsi$gpa, breaks = c(2.0, 2.5, 3.0, 3.5, 4.0, 4.5), xlab="GPA", ylab="N of students", main="GPA of students not receiving PSI")
 hist(usepsi$gpa, breaks = c(2.0, 2.5, 3.0, 3.5, 4.0, 4.5), xlab="GPA", ylab="N of students", main="GPA of students receiving PSI")
@@ -46,7 +51,8 @@ summary(expsiglm2)
 
 #T5
 
-
+#exp(intercept(which is supposedly psi0) − psi1?) = 1.786932. )
+  
 
 #T6
 
@@ -60,9 +66,15 @@ fisher.test(x)
 #So first column is students not receiving psi and second is the students receiving it
 #row 1 shows the students that improved and row 2 the rest that didn't
 
+#conclusion: #with pval 0.02 we can reject the null hypothesis, 
+#which will be that the students receiving psi and the 
+#ones not receiving it have the same probablity of improvement.
 
 #T7
 
-
+#I guess it's okay? Fishers test works with this kind of experiments
 
 #T8
+
+#Fishers is good for small counts in each 2x2 table cell. 
+#More exact than the other approach, ut it doesn't work with  big counts.
