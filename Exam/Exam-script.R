3593e4ca139cf37df94377aba5374c2982c096a9
tanjina.islam <t.islam@student.vu.nl>
2018-03-21 16:03:38 +0100

    exam task 3

--- Exam/Exam-script.R
+++ Exam/Exam-script.R
@@ -1,5 +1,6 @@
 #EXAM!!!!!!
 
+# Task 1
 gala_data = read.table("gala.txt", header = TRUE)
 gala_data
 attach(gala_data)
@@ -14,31 +15,30 @@ summary(gala_glm)
 
 Species = 3.15 - 5.80e-04 * Area +   3.54e-03  * Elevation + 8.83e-03 * Nearest - 5.71e-03 * Scruz - 6.63e-04 * Adjacent + error
 
-######################## END #########################################
-## For lm
-# Nearest p = 0.9932 > 0.05
-# Omit it
+######################## END  Task 1 #########################################
 
-# Step Down
+## Task 3
 
-gala_glm_sd = glm(Species~Area+Elevation+Scruz+Adjacent, family= poisson, data = gala_data)
-summary(gala_glm_sd)
+## Plots: Fitted vs Residuals
 
-# Area p = 0.27555 > 0.05
-# Omit it
+plot(fitted(gala_glm), residuals(gala_glm)) 
 
-gala_glm_sd = glm(Species~Elevation+Scruz+Adjacent, family= poisson, data = gala_data)
-summary(gala_glm_sd)
+# No specific pattern
+# It's a scatter plot not visually good as it would suppose to be in linear regression model.
 
-# Scruz p = 0.19632 > 0.05
-# Omit it
 
-gala_glm_sd = glm(Species~Elevation+Adjacent, family= poisson, data = gala_data)
-summary(gala_glm_sd)
+## Plots: Logarithmic-Fitted values vs Residuals
 
-# As we can see, all the p-values are smaller than 0.05 now
-# That means all the variables are significant for our model.
+plot(log(fitted(gala_glm)), residuals(gala_glm))
 
-# The resulting model of the step-down method is:
+# So we took logarithm to make the x-values fitted by a linear function in the plot. 
+# This plot seems OK yet still no specific pattern
+# And still it looks scatter too!
 
-Species = 1.4329 + 0.2766 * Elevation - 0.0689 * Adjacent + error
+## Plots: Logarithmic-Fitted values vs Residuals with type = Response
+
+plot(log(fitted(gala_glm)), residuals(gala_glm, type = "response"))
+
+## The response residuals increase with log of the fitted values as expected from Poisson model
+
+######################## END  Task 3 #########################################
